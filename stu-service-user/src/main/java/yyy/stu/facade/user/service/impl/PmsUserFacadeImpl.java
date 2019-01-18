39c253a51d67ee607340ba2f593ce4eca13b079e
yuyy76 <dl_yyy@sina.com>
2019-01-18 14:17:55 +0800

    20190118-14:18

---
+++ stu-service-user/src/main/java/yyy/stu/facade/user/service/impl/PmsUserFacadeImpl.java
@@ -0,0 +1,98 @@
+/**
+ * 
+ */
+package yyy.stu.facade.user.service.impl;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import yyy.stu.facade.user.common.page.PageBean;
+import yyy.stu.facade.user.common.page.PageParam;
+import yyy.stu.facade.user.entity.PmsUser;
+import yyy.stu.facade.user.service.PmsUserFacade;
+import yyy.stu.service.user.biz.PmsUserBiz;
+
+/**
+ * @描述：用户dubbo服务接口实现
+ * @author yyy
+ *
+ */
+@Service("PmsUserFacade")
+public class PmsUserFacadeImpl implements PmsUserFacade {
+
+	@Autowired
+	private PmsUserBiz pmsUserBiz;
+	
+	/**
+	 * 保存用户信息.
+	 * @param pmsUser
+	 */
+	public void create(PmsUser pmsUser) {
+		pmsUserBiz.create(pmsUser);
+	}
+	
+	/**
+	 * 根据ID获取用户信息.
+	 * @param userId
+	 * @return
+	 */
+	public PmsUser getById(Long userId) {
+		return pmsUserBiz.getById(userId);
+	}
+
+	/**
+	 * 根据登录名取得用户对象
+	 */
+	public PmsUser findUserByUserNo(String userNo) {
+		return pmsUserBiz.findUserByUserNo(userNo);
+	}
+
+	/**
+	 * 根据ID删除一个用户，同时删除与该用户关联的角色关联信息. type="1"的超级管理员不能删除.
+	 * 
+	 * @param id
+	 *            用户ID.
+	 */
+	public void deleteUserById(long userId) {
+		pmsUserBiz.deleteUserById(userId);
+	}
+
+	
+	/**
+	 * 更新用户信息.
+	 * @param user
+	 */
+	public void update(PmsUser user) {
+		pmsUserBiz.update(user);
+	}
+	
+	/**
+	 * 根据用户ID更新用户密码.
+	 * 
+	 * @param userId
+	 * @param newPwd
+	 *            (已进行SHA1加密)
+	 */
+	public void updateUserPwd(Long userId, String newPwd, boolean isTrue) {
+		pmsUserBiz.updateUserPwd(userId, newPwd, isTrue);
+	}
+
+
+
+	/**
+	 * 查询并分页列出用户信息.
+	 * @param pageParam
+	 * @param paramMap
+	 * @return
+	 */
+	public PageBean listPage(PageParam pageParam, Map<String, Object> paramMap) {
+		return pmsUserBiz.listPage(pageParam, paramMap);
+	}
+
+	
+	
+	
+	
+}
---
+++ stu-web-boss/src/main/java/yyy/stu/web/boss/base/UserLoginedAware.java
@@ -0,0 +1,15 @@
+package yyy.stu.web.boss.base;
+
+import yyy.stu.facade.user.entity.PmsUser;
+
+
+
+public interface UserLoginedAware {
+
+	/**
+	 * 取得登录的用户
+	 * 
+	 * @return
+	 */
+	public PmsUser getLoginedUser();
+}
